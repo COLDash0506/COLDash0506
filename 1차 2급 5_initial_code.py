4bbc392b8c1f486ccae11ca9d287163dc2c714e9
Dev_COLDash <113005599+COLDash0506@users.noreply.github.com>
2023-09-09 14:10:17 +0900

    Add files via upload

---
+++ 1차 2급 5_initial_code.py
@@ -0,0 +1,10 @@
+def solution(arr):
+
+    return list(reversed(arr))
+
+#The following is code to output testcase.
+arr = [1, 4, 2, 3]
+ret = solution(arr)
+
+#Press Run button to receive output.
+print("Solution: return value of the function is ", ret, " .")
